7042c55f7df7b1e058dd1af0bd5916639e30440f
jerr <jerry@fam-base.net>
2018-05-16 15:55:46 -0400

    ver

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2014-2016 SDN developers
+// Copyright (c) 2016 Bitcedi developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x3D, 0x1E, 0x32, 0x33, 0x15, 0xA6, 0x52, 0x5A, 0x3B, 0x70, 0x4E, 0x7C, 0x28, 0x5D, 0x63, 0x7D } };
+}
