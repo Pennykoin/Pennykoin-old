008ae10b65a25190fba4f289099697c88405e19a
Jerry Howell <39189962+Pennykoin@users.noreply.github.com>
2018-05-21 14:19:02 -0400

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -157,7 +157,7 @@ __attribute__((unused))
 const std::initializer_list<CheckpointData> CHECKPOINTS = {
 
   { 680, "b5c5635f1de8934468b35c5e4ae48f4fc6e47ae21d85d0e1b563835f2b40f88d" },
-  { 1550, "a01ca16cb141e1a20b6c45651e124660117180503369cbc4f89f82a544a44720" },
+  { 1550, "3576f6075b79223a3bbf9b7404b0a0687eb6f20b7cec0c07ddbcc6ed8ae59430" },
   {4145, "3eb7f65c24bb414684de610c863cbd4bf6a1638746361d8db1a5ef03eb2d0690"}
 
 };
